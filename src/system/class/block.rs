e3f53cf1867bdba450b8bb7bf7af6b80846a1dba
Diana <5275194+DianaNites@users.noreply.github.com>
2022-10-21 08:51:00 -0700

    wip

--- src/system/class/block.rs
+++ src/system/class/block.rs
@@ -9,23 +9,71 @@
 //!
 //! [1]: https://www.kernel.org/doc/Documentation/ABI/stable/sysfs-block
 //! [2]: https://www.kernel.org/doc/Documentation/ABI/testing/sysfs-block
-
+#![allow(unused_variables, unused_imports, clippy::all, dead_code, unused_mut)]
 use std::{
     convert::TryFrom,
-    ffi::OsStr,
     io,
     path::{Path, PathBuf},
 };
 
-use super::{Device, GenericDevice};
-
-// use super::Device;
+use super::{Device, GenericDevice, SYSFS_PATH};
 
 /// A linux block device
 #[derive(Debug, Clone)]
 pub struct Block {
     /// Canonical, full, path to the device.
     path: PathBuf,
+}
+
+impl Block {
+    fn new(path: PathBuf) -> Self {
+        Self { path }
+    }
+
+    /// Get connected Block Devices, sorted.
+    ///
+    /// # Note
+    ///
+    /// Partitions are **not** included. Use [`Block::partitions`].
+    ///
+    /// # Errors
+    ///
+    /// - If unable to read any of the subsystem directories.
+    pub fn devices() -> io::Result<Vec<Self>> {
+        let sysfs = Path::new(SYSFS_PATH);
+        let mut devices = Vec::new();
+        // Have to check both paths
+        let paths = if sysfs.join("subsystem").exists() {
+            vec![sysfs.join("subsystem/block/devices")]
+        } else {
+            vec![sysfs.join("class/block"), sysfs.join("block")]
+        };
+        for path in paths {
+            if !path.exists() {
+                continue;
+            }
+            for dev in path.read_dir()? {
+                let dev = dev?;
+                let path = dev.path();
+                let dev = path.read_link()?;
+                let mut c = dev.components();
+                for p in c.by_ref() {
+                    if p.as_os_str() == "devices" {
+                        break;
+                    }
+                }
+                devices.push(Self::new(
+                    Path::new(SYSFS_PATH).join("devices").join(c.as_path()),
+                ));
+            }
+        }
+        devices.sort_unstable_by(|a: &Block, b| a.path.cmp(&b.path));
+        devices.dedup_by(|a, b| a.path == b.path);
+        // Remove sub-devices, partitions in this context.
+        devices.dedup_by(|a, b| a.path.starts_with(&b.path));
+
+        Ok(devices)
+    }
 }
 
 impl Device for Block {
@@ -46,4 +94,18 @@ impl TryFrom<GenericDevice> for Block {
     }
 }
 
-// pub struct TryFromDeviceError {}
+#[cfg(test)]
+mod tests {
+    #![allow(unreachable_code)]
+    use super::*;
+
+    type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+    #[test]
+    fn devices() -> Result<()> {
+        let devices = Block::devices();
+        dbg!(&devices);
+        panic!();
+        Ok(())
+    }
+}
