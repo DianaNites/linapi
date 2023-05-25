2e6272fcdfabaa3bb7da3be054db7c15b5f698e7
Diana <5275194+DianaNites@users.noreply.github.com>
2023-05-24 18:44:15 -0700

    kernel_info

--- src/system.rs
+++ src/system.rs
@@ -1,6 +1,8 @@
 //! This module provides ways to get information about a running Linux system
 use std::collections::HashMap;
 
+use rustix::process::{uname as r_uname, Uname as RUname};
+
 pub mod devices;
 pub mod modules;
 
@@ -23,4 +25,54 @@ pub trait UEvent {
 
     /// Return the Key=Value pairs in the `uevent` file.
     fn read(&self) -> HashMap<String, String>;
+}
+
+/// Holds information about the current Kernel
+pub struct Info(RUname);
+
+impl Info {
+    /// Operating System name
+    #[doc(alias = "sysname")]
+    pub fn sys(&self) -> &str {
+        self.0.sysname().to_str().expect("non-ascii uname")
+    }
+
+    /// Network / Node / Host name
+    #[doc(alias = "hostname")]
+    pub fn host(&self) -> &str {
+        self.0.nodename().to_str().expect("non-ascii uname")
+    }
+
+    /// Domain name
+    #[doc(alias = "domainname")]
+    pub fn domain(&self) -> &str {
+        self.0.domainname().to_str().expect("non-ascii uname")
+    }
+
+    /// OS-specific release/version information
+    #[doc(alias = "version")]
+    pub fn info(&self) -> &str {
+        self.0.version().to_str().expect("non-ascii uname")
+    }
+
+    /// OS release version
+    pub fn release(&self) -> &str {
+        self.0.release().to_str().expect("non-ascii uname")
+    }
+
+    /// Hardware architecture
+    pub fn machine(&self) -> &str {
+        self.0.machine().to_str().expect("non-ascii uname")
+    }
+}
+
+/// Get information about the current kernel
+///
+/// # Implementation
+///
+/// This uses `uname(2)`
+#[inline]
+#[doc(alias = "uname")]
+pub fn kernel_info() -> Info {
+    Info(r_uname())
 }
